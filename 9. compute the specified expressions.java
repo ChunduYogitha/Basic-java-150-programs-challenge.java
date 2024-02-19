ac1114749729c7adcf0a60abb6cde4d47f5bd7a8
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-02-19 17:06:37 +0530

    9. compute the specified expressions.java

---
+++ 9. compute the specified expressions.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main{
+  public static void main(String[] args){
+    
+    System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+  }
+}
+
+output
+  2.138888888888889
