1d61e79e99223359efe28b906b02118c30286eab
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-02-19 17:00:05 +0530

    7.multiplication of table.java

---
+++ 7.multiplication of table.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class Main{
+  public static void main(String[] args){
+    int num1 = 8;
+    for (int i=0; i<10; i++){
+      System.out.println(num1+ " * " + (i+1) + " = " + (num1*(i+1)));
+    } 
+  }
+}
+
+output
+  8 * 1 = 8
+  8 * 2 = 16
+  8 * 3 = 24
+  8 * 4 = 32
+  8 * 5 = 40
+  8 * 6 = 48
+  8 * 7 = 56
+  8 * 8 = 64
+  8 * 9 = 72
+  8 * 10 = 80
+  
