e5056d62abfce41a7669eb06e9d0bba78e1b16d2
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-02-19 17:09:38 +0530

    10.compute a specified formula.java

---
+++ 10.compute a specified formula.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Main{
+  public static void main(String[] args){
+    
+    System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+  }
+}
+output
+  2.9760461760461765
