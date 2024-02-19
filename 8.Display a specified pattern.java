756bfa2af1884306d29b25c9ca826d9d0e82e60d
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-02-19 17:29:56 +0530

    8.Display a specified pattern.java

---
+++ 8.Display a specified pattern.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Main{
+  public static void main(String[] args){
+    
+    System.out.println("   J     a    v      v     a");
+    System.out.println("   J    a a    v    v    a  a");
+    System.out.println("J  J   aaaaa    v  v     aaaaa");
+    System.out.println(" JJ   a     a     v     a     a");
+  }
+}
+output
+   J     a    v      v    a
+   J    a a    v    v    a a
+J  J   aaaaa    v  v    aaaaa
+ JJ   a     a     v     a    a
+
