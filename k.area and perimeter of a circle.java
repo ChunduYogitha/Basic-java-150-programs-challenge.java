0966c50ac63e9b6ab04f2638ec42df0260662f98
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-02-20 11:14:14 +0530

    k.area and perimeter of a circle.java

---
+++ k.area and perimeter of a circle.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Main{
+  public static void main(String[] args){
+    double r = 7.5;
+    System.out.println("perimeter is =  " + 2*(3.14*r));
+    System.out.println("Area is = " + 3.14*(r*r));
+  }
+}
+
+Output
+  perimeter is =  47.1
+  Area is = 176.625
